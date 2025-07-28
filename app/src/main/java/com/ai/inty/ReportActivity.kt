f4def3c69e9b05de48af24f827154de1b2cdd36e
GoFeature <gzw@sxwl.ai>
2025-07-28 10:14:55 +0800

    举报页面的键盘滚动toolbar移出问题处理

--- app/src/main/java/com/ai/inty/ReportActivity.kt
+++ app/src/main/java/com/ai/inty/ReportActivity.kt
@@ -17,6 +17,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
@@ -24,7 +25,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
@@ -47,7 +47,6 @@ import com.ai.inty.base.IntyImage
 import com.ai.inty.base.IntySmallTextField2
 import com.ai.inty.base.noRippleClickable
 import com.ai.inty.beans.ReportItem
-import com.ai.inty.ui.theme.BackGround
 import com.ai.inty.ui.theme.IntyTheme
 import com.ai.inty.viewmodels.ReportViewModel
 import com.inty.utils.log.EasyLog
@@ -146,7 +145,7 @@ private fun ReportScreen(
 ) {
     val focusManager = LocalFocusManager.current
 
-    Scaffold(
+    Box(
         modifier = Modifier
             .fillMaxSize()
             .clickable(
@@ -155,254 +154,239 @@ private fun ReportScreen(
             ) {
                 focusManager.clearFocus()
             },
-        containerColor = BackGround,
-        topBar = {
-            CenterAlignedTopAppBar(
-                colors = TopAppBarDefaults.centerAlignedTopAppBarColors()
-                    .copy(containerColor = Color.Transparent),
-                title = {
-                    Text(
-                        text = stringResource(R.string.report),
-                        color = Color.White,
-                        fontWeight = FontWeight.SemiBold,
-                        fontSize = 20.sp,
-                    )
-                },
-                navigationIcon = {
-                    Image(
-                        modifier = Modifier
-                            .padding(horizontal = 12.dp)
-                            .noRippleClickable {
-                                onBack()
-                            },
-                        painter = painterResource(R.drawable.back),
-                        contentDescription = null,
-                    )
+    ) {
 
-                },
-
-                )
-        },
-        bottomBar = {
-            Column {
-                SaveBtn(onSave = onSave)
-                Spacer(Modifier.height(60.dp))
-            }
-        }
-    ) { paddingValues ->
-
-
-        val scrollState = rememberScrollState()
 
         Column(
             modifier = Modifier
-                .padding(paddingValues)
-//                .imePadding()
+                .matchParentSize()
+                .padding(horizontal = 16.dp)
+                .imePadding()
+                .verticalScroll(rememberScrollState()),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
+
+            Spacer(Modifier.height(100.dp))
+
+            Column(
+                modifier = Modifier
+                    .background(
+                        color = Color(0x1A78599A),
+                        shape = RoundedCornerShape(8.dp)
+                    )
+                    .border(
+                        brush = Brush.linearGradient(
+                            colors = listOf(
+                                Color.Transparent,
+                                Color.White.copy(0.2f),
+                                Color.Transparent
+                            )
+                        ),
+                        width = 1.dp,
+                        shape = RoundedCornerShape(8.dp)
+                    )
+                    .padding(horizontal = 12.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Spacer(Modifier.height(16.dp))
+
+                Text(
+                    text = "NPC *",
+                    modifier = Modifier.fillMaxWidth(),
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.SemiBold,
+                    color = Color.White,
+                )
+                Spacer(Modifier.height(12.dp))
+
+                reasons.forEach { reason ->
+                    val isSelected = selectIDs.contains(reason.id)
+                    ReportItem(
+                        text = reason.description,
+                        selected = isSelected,
+                        onClick = {
+                            onClickReason(reason.id, !isSelected)
+                        }
+                    )
+                }
+
+                Spacer(Modifier.height(14.dp))
+            }
+
+            Spacer(Modifier.height(24.dp))
+
+
             Column(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .weight(1f)
-                    .verticalScroll(scrollState),
-                horizontalAlignment = Alignment.CenterHorizontally
+                    .background(
+                        color = Color(0x1A78599A),
+                        shape = RoundedCornerShape(8.dp)
+                    )
+                    .border(
+                        brush = Brush.linearGradient(
+                            colors = listOf(
+                                Color.Transparent,
+                                Color.White.copy(0.2f),
+                                Color.Transparent
+                            )
+                        ),
+                        width = 1.dp,
+                        shape = RoundedCornerShape(8.dp)
+                    )
+                    .padding(horizontal = 12.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
             ) {
+                Spacer(Modifier.height(16.dp))
+                Text(
+                    modifier = Modifier.fillMaxWidth(),
+                    text = "Report description *",
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.SemiBold,
+                    color = Color.White,
+                )
+                Spacer(Modifier.height(12.dp))
 
-                Spacer(Modifier.height(8.dp))
-
-                Column(
-                    modifier = Modifier
-                        .background(
-                            color = Color(0x1A78599A),
-                            shape = RoundedCornerShape(8.dp)
-                        )
-                        .border(
-                            brush = Brush.linearGradient(
-                                colors = listOf(
-                                    Color.Transparent,
-                                    Color.White.copy(0.2f),
-                                    Color.Transparent
-                                )
-                            ),
-                            width = 1.dp,
-                            shape = RoundedCornerShape(8.dp)
-                        )
-                        .padding(horizontal = 12.dp),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Spacer(Modifier.height(16.dp))
-
-                    Text(
-                        text = "NPC *",
-                        modifier = Modifier.fillMaxWidth(),
-                        fontSize = 14.sp,
-                        fontWeight = FontWeight.SemiBold,
-                        color = Color.White,
-                    )
-                    Spacer(Modifier.height(12.dp))
-
-                    reasons.forEach { reason ->
-                        val isSelected = selectIDs.contains(reason.id)
-                        ReportItem(
-                            text = reason.description,
-                            selected = isSelected,
-                            onClick = {
-                                onClickReason(reason.id, !isSelected)
-                            }
-                        )
-                    }
-
-                    Spacer(Modifier.height(14.dp))
-                }
-
-                Spacer(Modifier.height(24.dp))
-
-
-                Column(
+                Box(
                     modifier = Modifier
                         .fillMaxWidth()
+                        .height(112.dp)
                         .background(
-                            color = Color(0x1A78599A),
+                            color = Color.White.copy(0.1f),
                             shape = RoundedCornerShape(8.dp)
                         )
-                        .border(
-                            brush = Brush.linearGradient(
-                                colors = listOf(
-                                    Color.Transparent,
-                                    Color.White.copy(0.2f),
-                                    Color.Transparent
-                                )
-                            ),
-                            width = 1.dp,
-                            shape = RoundedCornerShape(8.dp)
-                        )
-                        .padding(horizontal = 12.dp),
-                    horizontalAlignment = Alignment.CenterHorizontally,
+                        .padding(vertical = 10.dp)
                 ) {
-                    Spacer(Modifier.height(16.dp))
-                    Text(
-                        modifier = Modifier.fillMaxWidth(),
-                        text = "Report description *",
-                        fontSize = 14.sp,
-                        fontWeight = FontWeight.SemiBold,
-                        color = Color.White,
-                    )
-                    Spacer(Modifier.height(12.dp))
-
-                    Box(
+                    IntySmallTextField2(
                         modifier = Modifier
-                            .fillMaxWidth()
-                            .height(112.dp)
-                            .background(
-                                color = Color.White.copy(0.1f),
-                                shape = RoundedCornerShape(8.dp)
-                            )
-                            .padding(vertical = 10.dp)
-                    ) {
-                        IntySmallTextField2(
-                            modifier = Modifier
-                                .fillMaxSize()
-                                .padding(horizontal = 16.dp),
-                            value = description,
-                            placeholder = {
-                                Text(
-                                    modifier = Modifier
-                                        .matchParentSize()
-                                        .align(Alignment.TopStart),
-                                    text = "Please fill in the feedback content...",
-                                    fontWeight = FontWeight.Normal,
-                                    color = Color.White.copy(0.55f),
-                                    fontSize = 14.sp,
-                                )
-                            },
-                            onValueChange = {
-                                onDescriptionChange(it)
-                            }
-                        )
-
-                        Text(
-                            modifier = Modifier
-                                .align(Alignment.BottomEnd)
-                                .padding(horizontal = 12.dp),
-                            text = "${description.length}/400",
-                            fontSize = 12.sp,
-                            color = Color.White.copy(0.55f),
-                        )
-                    }
-                    Spacer(Modifier.height(16.dp))
-                }
-
-
-                Spacer(Modifier.height(24.dp))
-
-
-                Column(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .background(
-                            color = Color(0x1A78599A),
-                            shape = RoundedCornerShape(8.dp)
-                        )
-                        .border(
-                            brush = Brush.linearGradient(
-                                colors = listOf(
-                                    Color.Transparent,
-                                    Color.White.copy(0.2f),
-                                    Color.Transparent
-                                )
-                            ),
-                            width = 1.dp,
-                            shape = RoundedCornerShape(8.dp)
-                        )
-                        .padding(horizontal = 12.dp),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Spacer(Modifier.height(16.dp))
-                    Text(
-                        modifier = Modifier.fillMaxWidth(),
-                        text = "Image evidence",
-                        fontSize = 14.sp,
-                        fontWeight = FontWeight.SemiBold,
-                        color = Color.White,
-                    )
-                    Spacer(Modifier.height(12.dp))
-
-                    Box(
-                        modifier = Modifier
-                            .size(88.dp)
-                            .align(Alignment.Start)
-                            .background(
-                                color = Color.White.copy(0.1f),
-                                shape = RoundedCornerShape(8.dp)
-                            )
-                            .clip(RoundedCornerShape(8.dp)),
-                    ) {
-                        if (images.isNotEmpty()) {
-                            IntyImage(
-                                modifier = Modifier.fillMaxSize(),
-                                model = images.firstOrNull(),
-                            )
-                        } else {
-                            Image(
+                            .fillMaxSize()
+                            .padding(horizontal = 16.dp),
+                        value = description,
+                        placeholder = {
+                            Text(
                                 modifier = Modifier
-                                    .size(26.dp)
-                                    .align(Alignment.Center)
-                                    .noRippleClickable {
-                                        onClickAddImage()
-                                    },
-                                painter = painterResource(R.drawable.btn_add6),
-                                contentDescription = null,
+                                    .matchParentSize()
+                                    .align(Alignment.TopStart),
+                                text = "Please fill in the feedback content...",
+                                fontWeight = FontWeight.Normal,
+                                color = Color.White.copy(0.55f),
+                                fontSize = 14.sp,
                             )
+                        },
+                        onValueChange = {
+                            onDescriptionChange(it)
                         }
-                    }
+                    )
 
-                    Spacer(Modifier.height(16.dp))
+                    Text(
+                        modifier = Modifier
+                            .align(Alignment.BottomEnd)
+                            .padding(horizontal = 12.dp),
+                        text = "${description.length}/400",
+                        fontSize = 12.sp,
+                        color = Color.White.copy(0.55f),
+                    )
+                }
+                Spacer(Modifier.height(16.dp))
+            }
+
+
+            Spacer(Modifier.height(24.dp))
+
+
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(
+                        color = Color(0x1A78599A),
+                        shape = RoundedCornerShape(8.dp)
+                    )
+                    .border(
+                        brush = Brush.linearGradient(
+                            colors = listOf(
+                                Color.Transparent,
+                                Color.White.copy(0.2f),
+                                Color.Transparent
+                            )
+                        ),
+                        width = 1.dp,
+                        shape = RoundedCornerShape(8.dp)
+                    )
+                    .padding(horizontal = 12.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Spacer(Modifier.height(16.dp))
+                Text(
+                    modifier = Modifier.fillMaxWidth(),
+                    text = "Image evidence",
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.SemiBold,
+                    color = Color.White,
+                )
+                Spacer(Modifier.height(12.dp))
+
+                Box(
+                    modifier = Modifier
+                        .size(88.dp)
+                        .align(Alignment.Start)
+                        .background(
+                            color = Color.White.copy(0.1f),
+                            shape = RoundedCornerShape(8.dp)
+                        )
+                        .clip(RoundedCornerShape(8.dp)),
+                ) {
+                    if (images.isNotEmpty()) {
+                        IntyImage(
+                            modifier = Modifier.fillMaxSize(),
+                            model = images.firstOrNull(),
+                        )
+                    } else {
+                        Image(
+                            modifier = Modifier
+                                .size(26.dp)
+                                .align(Alignment.Center)
+                                .noRippleClickable {
+                                    onClickAddImage()
+                                },
+                            painter = painterResource(R.drawable.btn_add6),
+                            contentDescription = null,
+                        )
+                    }
                 }
 
-//            Spacer(Modifier.height(60.dp))
-
+                Spacer(Modifier.height(16.dp))
             }
+
+            Spacer(Modifier.height(60.dp))
+
         }
 
+
+        CenterAlignedTopAppBar(
+            colors = TopAppBarDefaults.centerAlignedTopAppBarColors()
+                .copy(containerColor = Color(0XFF1C1523)),
+            title = {
+                Text(
+                    text = stringResource(R.string.report),
+                    color = Color.White,
+                    fontWeight = FontWeight.SemiBold,
+                    fontSize = 20.sp,
+                )
+            },
+            navigationIcon = {
+                Image(
+                    modifier = Modifier
+                        .padding(horizontal = 12.dp)
+                        .noRippleClickable {
+                            onBack()
+                        },
+                    painter = painterResource(R.drawable.back),
+                    contentDescription = null,
+                )
+
+            },
+
+            )
     }
 }
 
