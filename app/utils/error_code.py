72613898b963cfce099df8043555306ecf05803f
Yaxiong Zhao <z@sxwl.ai>
2025-08-04 17:24:27 -0700

    Add error code doc.

--- app/utils/error_code.py
+++ app/utils/error_code.py
@@ -1,7 +1,13 @@
 """
-Error codes for API responses.
-The App side can use these codes to determine exact behaviors.
+Error codes used in API responses to app (or client side).
+The app side can use these codes to determine exact behaviors.
 For example, app can recommend subscription when a free user reaches the limit.
+
+Certain information is hidden from the app side to avoid leaking sensitive information,
+and to avoid confusing users.
+
+The app should be checking the error code, to determine if they need to show 
+detailed error message for display.
 """
 
 from enum import StrEnum
