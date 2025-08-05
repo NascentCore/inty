c77d18ab305e5516f3659530d39f2f0ca5584dae
ZYX <z@sxwl.ai>
2025-08-04 19:29:57 -0700

    Update and rename models.txt to models.py

--- experimental/comfyui/models.py
+++ experimental/comfyui/models.py
@@ -1,2 +1,3 @@
 # List of models to be downloed from civitai
 # Used for populating models onto gcs bucket and then transfer to runpod network volume
+https://civitai.com/api/download/models/1918677?type=Model&format=SafeTensor, 
