8474ba4193bbbd6e11cc1e2642054619fabe077d
Yaxiong Zhao <z@sxwl.ai>
2025-08-19 23:06:00 -0700

    Add openai_client.py

---
+++ app/utils/openai_client.py
@@ -0,0 +1,50 @@
+"""
+Wrapper of OpenAI API, used to wrap the OpenAI API with LangSmith.
+"""
+
+"""
+Demo for using OpenAI SDK with LangSmith to track the usage of OpenAI API.
+"""
+
+import os
+from openai import OpenAI
+from langsmith import wrappers
+
+
+from app.core.config import global_config_loaded_from_config_yaml
+
+
+def get_openai_client():
+    """
+    Return an OpenAI client with LangSmith tracing.
+    The ENV vars are required by langsmith.
+    """
+    # These env vars has no arguments inputable to langchina api
+    assert os.getenv("LANGCHAIN_API_KEY"), "LANGCHAIN_API_KEY must be set"
+    assert os.getenv("LANGSMITH_TRACING_V2"), "LANGSMITH_TRACING_V2 must be set"
+    assert os.getenv("LANGSMITH_PROJECT"), "LANGSMITH_PROJECT must be set"
+
+    # Create OpenAI client and wrap it with LangSmith
+    client = wrappers.wrap_openai(
+        OpenAI(
+            base_url=global_config_loaded_from_config_yaml.agent.base_url,
+            api_key=global_config_loaded_from_config_yaml.agent.api_key,
+        )
+    )
+
+    return client
+
+
+if __name__ == "__main__":
+    """
+    Test the openai client.
+    """
+    from dotenv import load_dotenv
+
+    load_dotenv()
+    client = get_openai_client()
+    response = client.chat.completions.create(
+        model="openai/gpt-3.5-turbo",
+        messages=[{"role": "user", "content": "Hello, world!"}],
+    )
+    print(response)
