67968d51242f7c0e8e2f084ff70b967e90595602
Yaxiong Zhao <z@sxwl.ai>
2025-08-23 23:54:08 -0700

    Fix
    Signed-off-by: Yaxiong Zhao <z@sxwl.ai>

--- app/models/user.py
+++ app/models/user.py
@@ -69,7 +69,7 @@ class User(Base):
     auth_type = Column(
         Enum(AuthType), nullable=False, comment="认证类型：手机号/Google/游客"
     )
-    google_id = Column(String, comment="Google账号ID，唯一，用于Google登录")
+    google_id = Column(String, comment="Google账号ID，用于支持用户注册登录")
     device_id = Column(String, unique=True, comment="设备ID，唯一，用于设备识别")
     system_language = Column(String, default="en", comment="系统语言偏好，默认英语")
     is_active = Column(Boolean, default=True, comment="账号是否激活")
