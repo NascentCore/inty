07e3668770a376879fabe729e2ef1238c5f6c8fe
GoFeature <gzw@sxwl.ai>
2025-08-27 15:01:23 +0800

    Merge pull request #141
    * 推荐agents接口添加sort排序字段

--- app/src/main/java/com/ai/inty/net/IAgentApi.kt
+++ app/src/main/java/com/ai/inty/net/IAgentApi.kt
@@ -24,35 +24,48 @@ import retrofit2.http.Query
 interface IAgentApi {
 
     @GET("api/v1/ai/agents/recommend")
-    suspend fun recommendAgents(@Query("page") page: Int, @Query("page_size")pageSize: Int): HttpResult<AgentInfoResponse>
-    
+    suspend fun recommendAgents(
+        @Query("page") page: Int,
+        @Query("page_size") pageSize: Int,
+        @Query("page") sort: String = "random",//三种排序 created_asc,created_desc,random
+    ): HttpResult<AgentInfoResponse>
+
     @GET("/api/v1/ai/agents/following")
-    suspend fun getFollowingAgents(@Query("page") page: Int, @Query("page_size") pageSize: Int): HttpResult<AgentInfoResponse>
-    
+    suspend fun getFollowingAgents(
+        @Query("page") page: Int,
+        @Query("page_size") pageSize: Int
+    ): HttpResult<AgentInfoResponse>
+
     @POST("/api/v1/ai/agents/{agentId}/follow")
     suspend fun followAgent(@Path("agentId") agentId: String): HttpResult<FollowResponse>
-    
+
     @DELETE("/api/v1/ai/agents/{agentId}/follow")
     suspend fun unfollowAgent(@Path("agentId") agentId: String): HttpResult<FollowResponse>
-    
+
     @POST("/api/v1/ai/agents/")
     suspend fun createAgent(@Body request: CreateAgentRequest): HttpResult<AgentInfo>
-    
+
     @GET("/api/v1/ai/agents/")
-    suspend fun getUserCreatedAgents(@Query("skip") skip: Int, @Query("limit") limit: Int): HttpResult<List<AgentInfo>>
-    
+    suspend fun getUserCreatedAgents(
+        @Query("skip") skip: Int,
+        @Query("limit") limit: Int
+    ): HttpResult<List<AgentInfo>>
+
     @POST("/api/v1/ai/agents/generate_background")
     suspend fun generateBackground(@Body request: GenerateBackgroundRequest): HttpResult<GenerateBackgroundResponse>
-    
+
     @GET("/api/v1/ai/agents/{agentId}")
     suspend fun getAgentDetail(@Path("agentId") agentId: String): HttpResult<AgentInfo>
-    
+
     @PUT("/api/v1/ai/agents/{agentId}")
-    suspend fun updateAgent(@Path("agentId") agentId: String, @Body request: CreateAgentRequest): HttpResult<AgentInfo>
-    
+    suspend fun updateAgent(
+        @Path("agentId") agentId: String,
+        @Body request: CreateAgentRequest
+    ): HttpResult<AgentInfo>
+
     @DELETE("/api/v1/ai/agents/{agentId}")
     suspend fun deleteAgent(@Path("agentId") agentId: String): HttpResult<AgentInfo>
-    
+
     @Multipart
     @POST("/api/v1/ai/agents/upload-avatar")
     suspend fun uploadAvatar(@Part file: MultipartBody.Part): HttpResult<UploadAvatarResponse>
