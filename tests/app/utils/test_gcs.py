af7e654a2ace9ed9b66af208fb3bfc049c6b9d16
Yaxiong Zhao <z@sxwl.ai>
2025-09-01 16:13:24 -0700

    Add append_filename_suffix

--- tests/app/utils/test_gcs.py
+++ tests/app/utils/test_gcs.py
@@ -1,10 +1,15 @@
+<<<<<<< HEAD
 from app.utils.gcs import append_filename_suffix, get_bucket_and_path_from_gcs_url
+=======
+from app.utils.gcs import append_filename_suffix, get_path_from_gcs_url
+>>>>>>> f8305cc1 (Add append_filename_suffix)
 from app.core.config import global_config_loaded_from_config_yaml
 
 
 def test_get_bucket_and_path_from_gcs_url():
     old_bucket = global_config_loaded_from_config_yaml.gcs.bucket
     global_config_loaded_from_config_yaml.gcs.bucket = "test-bucket"
+<<<<<<< HEAD
     assert (
         get_bucket_and_path_from_gcs_url(
             "https://storage.googleapis.com/test-bucket/test-path"
@@ -40,6 +45,15 @@ def test_get_bucket_and_path_from_gcs_url():
         )
         == "test-path/test-path2"
     )
+=======
+    assert get_path_from_gcs_url("https://storage.googleapis.com/test-bucket/test-path") == "test-path"
+    assert get_path_from_gcs_url("https://storage.googleapis.com/test-bucket/test-path") == "test-path"
+    assert get_path_from_gcs_url("gs://test-bucket/test-path") == "test-path"
+    assert get_path_from_gcs_url("https://storage.googleapis.com/test-bucket/test-path") == "test-path"
+    assert get_path_from_gcs_url("https://storage.googleapis.com/test-bucket/test-path/test-path2") == "test-path/test-path2"
+    assert get_path_from_gcs_url("gs://test-bucket/test-path/test-path2") == "test-path/test-path2"
+    assert get_path_from_gcs_url("https://storage.googleapis.com/test-bucket/test-path/test-path2") == "test-path/test-path2"
+>>>>>>> f8305cc1 (Add append_filename_suffix)
     global_config_loaded_from_config_yaml.gcs.bucket = old_bucket
 
 
