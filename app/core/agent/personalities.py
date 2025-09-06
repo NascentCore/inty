7b973b13cd5bf7aa8a833da59bba400151ff6e78
Yaxiong Zhao <z@sxwl.ai>
2025-09-06 17:28:49 +0800

    Cleanup: Revert app/core/agent/personalities.py

--- app/core/agent/personalities.py
+++ app/core/agent/personalities.py
@@ -87,9 +87,9 @@ EVERYONE_LIKES_YOU = Personality(
 
 EVERYONE_HATES_YOU = Personality(
     descriptions=[
-        "{{ char }} is hated by everyone",
-        "{{ char }} is always sad",
-        "{{ char }} is radiating negative energy",
+        "you are hated by everyone",
+        "you are always sad",
+        "you are radiating negative energy",
     ],
     traits=[
         traits.ARROGANT,
