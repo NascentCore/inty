ca62296664a38ab14201e89fdafddde59d5b2f9b
Yaxiong Zhao <z@sxwl.ai>
2025-09-07 10:07:21 +0800

    Fix

--- tests/app/utils/test_gcs.py
+++ tests/app/utils/test_gcs.py
@@ -1,46 +1,25 @@
+from google.cloud.storage import bucket
 from app.utils.gcs import append_filename_suffix, get_bucket_and_path_from_gcs_url
 from app.core.config import global_config_loaded_from_config_yaml
 
 
 def test_get_bucket_and_path_from_gcs_url():
-    old_bucket = global_config_loaded_from_config_yaml.gcs.bucket
-    global_config_loaded_from_config_yaml.gcs.bucket = "test-bucket"
-    assert (
-        get_bucket_and_path_from_gcs_url(
-            "https://storage.googleapis.com/test-bucket/test-path"
-        )
-        == "test-path"
-    )
-    assert (
-        get_bucket_and_path_from_gcs_url(
-            "https://storage.googleapis.com/test-bucket/test-path"
-        )
-        == "test-path"
-    )
-    assert get_bucket_and_path_from_gcs_url("gs://test-bucket/test-path") == "test-path"
-    assert (
-        get_bucket_and_path_from_gcs_url(
-            "https://storage.googleapis.com/test-bucket/test-path"
-        )
-        == "test-path"
-    )
-    assert (
-        get_bucket_and_path_from_gcs_url(
-            "https://storage.googleapis.com/test-bucket/test-path/test-path2"
-        )
-        == "test-path/test-path2"
-    )
-    assert (
-        get_bucket_and_path_from_gcs_url("gs://test-bucket/test-path/test-path2")
-        == "test-path/test-path2"
-    )
-    assert (
-        get_bucket_and_path_from_gcs_url(
-            "https://storage.googleapis.com/test-bucket/test-path/test-path2"
-        )
-        == "test-path/test-path2"
-    )
-    global_config_loaded_from_config_yaml.gcs.bucket = old_bucket
+    assert get_bucket_and_path_from_gcs_url("gs://test-bucket/test-path") == [
+        "test-bucket",
+        "test-path",
+    ]
+    assert get_bucket_and_path_from_gcs_url(
+        "https://storage.googleapis.com/test-bucket/test-path/test-path2"
+    ) == [
+        "test-bucket",
+        "test-path/test-path2",
+    ]
+    assert get_bucket_and_path_from_gcs_url(
+        "https://storage.cloud.google.com/test-bucket/test-path/test-path2"
+    ) == [
+        "test-bucket",
+        "test-path/test-path2",
+    ]
 
 
 def test_append_filename_suffix():
