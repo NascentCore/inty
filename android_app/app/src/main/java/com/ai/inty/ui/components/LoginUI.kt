24e29af6da04dc06ca8ab420fd249c0f3f91612b
Yaxiong Zhao <z@sxwl.ai>
2025-09-09 09:59:16 +0800

    Cleanup

--- android_app/app/src/main/java/com/ai/inty/ui/components/LoginUI.kt
+++ android_app/app/src/main/java/com/ai/inty/ui/components/LoginUI.kt
@@ -32,13 +32,9 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.unit.TextUnit
 import androidx.core.net.toUri
 
 
