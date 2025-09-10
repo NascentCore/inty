93bb56e6dfc3b4a0160f1586c44a736862ca037a
GoFeature <gzw@sxwl.ai>
2025-09-10 15:38:02 +0800

    修复api的endpoint的/这两个不能删除 (#410)

--- android_app/app/src/main/java/com/ai/inty/net/ICommonApi.kt
+++ android_app/app/src/main/java/com/ai/inty/net/ICommonApi.kt
@@ -11,7 +11,7 @@ import retrofit2.http.POST
 
 @Singleton
 interface ICommonApi {
-    @POST("/api/v1/report")
+    @POST("/api/v1/report/")
     suspend fun report(@Body req: ReportReq): ReportResponse
 
     @POST("api/v1/version/check")
