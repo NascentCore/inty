446272f494573352740d337131a83b9aec0ffbeb
Yaxiong Zhao <z@sxwl.ai>
2025-09-11 17:34:01 +0800

    Fix

--- android_app/app/src/main/java/com/ai/inty/net/NetServiceMgr.kt
+++ android_app/app/src/main/java/com/ai/inty/net/NetServiceMgr.kt
@@ -15,8 +15,6 @@ import com.squareup.moshi.DefaultIfNullFactory
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import com.therouter.inject.ServiceProvider
-import com.inty.api.client.IntyClient
-import com.inty.api.client.okhttp.IntyOkHttpClient
 import okhttp3.ConnectionPool
 import okhttp3.Dns
 import okhttp3.Interceptor
