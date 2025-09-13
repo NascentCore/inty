47924bf7263e9c7ff435884dbd53307169b11eba
Yaxiong Zhao <z@sxwl.ai>
2025-09-13 20:51:09 +0800

    Add ActivityItem

--- android_app/app/src/main/java/com/ai/inty/home/ActivityItem.kt
+++ android_app/app/src/main/java/com/ai/inty/home/ActivityItem.kt
@@ -1,3 +1,20 @@
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Text
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.ai.inty.R
+import com.ai.inty.base.IntyImage
+
 // Common constants for ActivityItem
 val ACTIVITY_ITEM_HEIGHT = 88.dp
 val ACTIVITY_ITEM_AVATAR_SIZE = 56.dp
