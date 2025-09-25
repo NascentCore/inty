a9601797534ac0f95c467c9782d3a4ed4aedc230
ZYX <z@sxwl.ai>
2025-09-25 21:56:06 +0800

    fix

--- fmt.sh
+++ fmt.sh
@@ -2,4 +2,5 @@
 
 ktfmt --kotlinlang-style android_app/*.{kt,kts} android_app/{app,build-logic} android_app/library/{network,utils}
 black app/ scripts/
-npx prettier --write evaluation/
+# 格式化包括 json md 等等
+npx prettier --write .
