7764b0487466412e736bacddf067aa8eab880503
ZYX <z@sxwl.ai>
2025-09-25 12:21:31 +0800

    Also commit changes
    Signed-off-by: ZYX <z@sxwl.ai>

--- scripts/generate_openapi_json.py
+++ scripts/generate_openapi_json.py
@@ -1,5 +1,6 @@
 import argparse
 import json
+import os
 
 from app.main import app
 
@@ -13,3 +14,11 @@ with open(args.output, "w") as f:
     f.write(json_str)
 
 print(f"OpenAPI JSON saved to {args.output}")
+
+# 调用 git 来提交本地改动
+import subprocess
+
+subprocess.run(["git", "add", args.output])
+this_filename = os.path.basename(__file__)
+subprocess.run(["git", "commit", "-m", f"使用 {this_filename} 更新 openapi.json"])
+subprocess.run(["git", "push"])
