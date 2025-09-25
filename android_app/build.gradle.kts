aa4346ab3250782eda7eb3be802e4d44508c4215
ZYX <z@sxwl.ai>
2025-09-25 20:51:12 +0800

    Update build.gradle.kts

--- android_app/build.gradle.kts
+++ android_app/build.gradle.kts
@@ -8,6 +8,5 @@ plugins {
     alias(libs.plugins.google.services) apply false
     alias(libs.plugins.firebase.crashlytics) apply false
     alias(libs.plugins.firebase.perf) apply false
-
     alias(libs.plugins.the.router) apply false
 }
