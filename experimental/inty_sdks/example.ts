6d4b9e54f9857ce29ddfdb0074e350d6e9c6db87
ZYX <z@sxwl.ai>
2025-09-25 21:49:10 +0800

    Format kotlin code

--- experimental/inty_sdks/example.ts
+++ experimental/inty_sdks/example.ts
@@ -1,12 +1,12 @@
-import Inty from 'inty';
-import dotenv from 'dotenv';
+import Inty from "inty";
+import dotenv from "dotenv";
 
 // Load .env file
 dotenv.config();
 
 const client = new Inty({
-  apiKey: process.env['INTY_API_KEY'], // This is the default and can be omitted
-  baseURL: "http://localhost:8000"
+  apiKey: process.env["INTY_API_KEY"], // This is the default and can be omitted
+  baseURL: "http://localhost:8000",
 });
 
 const agentsResponse = await client.api.v1.ai.agents.list();
@@ -14,7 +14,7 @@ console.log("Agents:", agentsResponse);
 
 const guestResponse = await client.api.v1.auth.createGuest({
   device_id: "example-device-id",
-  system_language: "en"
+  system_language: "en",
 });
 
 console.log("Guest:", guestResponse);
