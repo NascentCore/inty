139f5c2b292ff9c0226fe8fca2b04fa89c1264b2
ZYX <z@sxwl.ai>
2025-09-25 21:28:40 +0800

    Format kotlin code

--- fmt.sh
+++ fmt.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-ktfmt --kotlinlang-style android_app/app/src
+ktfmt --kotlinlang-style android_app/{app,build-logic} android_app/library/{network,utils} android_app/library/*
 black app/ scripts/
 npx prettier --write evaluation/
