8348441a55d097f34f439b6e05c34e0aace49240
ZYX <z@sxwl.ai>
2025-09-26 21:42:46 +0800

    Fix
    Signed-off-by: ZYX <z@sxwl.ai>

--- update_inty_sdk_submodule.sh
+++ update_inty_sdk_submodule.sh
@@ -22,7 +22,7 @@ if [ -n "$(git status --porcelain)" ]; then
     git commit -m "更新 $INTY_SDK_KOTLIN_DIR submodule"
 fi
 
-INTY_SDK_TYPESCRIPT_DIR=sdks/typescript
+INTY_SDK_TYPESCRIPT_DIR=evaluation/inty_sdk
 pushd $INTY_SDK_TYPESCRIPT_DIR
 git checkout main
 git pull
