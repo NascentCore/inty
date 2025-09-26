f4c026072e7770d1eb55fae4bb0bcf878ec86e51
ZYX <z@sxwl.ai>
2025-09-26 21:41:50 +0800

    Fix
    Signed-off-by: ZYX <z@sxwl.ai>

--- update_inty_sdk_submodule.sh
+++ update_inty_sdk_submodule.sh
@@ -5,21 +5,30 @@ pushd $INTY_SDK_PYTHON_DIR
 git checkout main
 git pull
 popd
-git add $INTY_SDK_PYTHON_DIR
-git commit -m "更新 $INTY_SDK_PYTHON_DIR submodule"
+# commit if there are changes
+if [ -n "$(git status --porcelain)" ]; then
+    git add $INTY_SDK_PYTHON_DIR
+    git commit -m "更新 $INTY_SDK_PYTHON_DIR submodule"
+fi
 
 INTY_SDK_KOTLIN_DIR=android_app/library/inty_sdk
 pushd $INTY_SDK_KOTLIN_DIR
 git checkout main
 git pull
 popd
-git add $INTY_SDK_KOTLIN_DIR
-git commit -m "更新 $INTY_SDK_KOTLIN_DIR submodule"
+# commit if there are changes
+if [ -n "$(git status --porcelain)" ]; then
+    git add $INTY_SDK_KOTLIN_DIR
+    git commit -m "更新 $INTY_SDK_KOTLIN_DIR submodule"
+fi
 
 INTY_SDK_TYPESCRIPT_DIR=sdks/typescript
 pushd $INTY_SDK_TYPESCRIPT_DIR
 git checkout main
 git pull
 popd
-git add $INTY_SDK_TYPESCRIPT_DIR
-git commit -m "更新 $INTY_SDK_TYPESCRIPT_DIR submodule"
+# commit if there are changes
+if [ -n "$(git status --porcelain)" ]; then
+    git add $INTY_SDK_TYPESCRIPT_DIR
+    git commit -m "更新 $INTY_SDK_TYPESCRIPT_DIR submodule"
+fi
