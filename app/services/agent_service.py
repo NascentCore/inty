fb31bb1b34ca67847ddc2d88e6cb4713d45d8b59
ZYX <z@sxwl.ai>
2025-09-27 09:10:11 +0800

    refactor

--- app/services/agent_service.py
+++ app/services/agent_service.py
@@ -651,7 +651,6 @@ async def get_recommended_agents_paginated(
                     and_(
                         models.Agent.visibility == AgentVisibility.PUBLIC,
                         models.Agent.deleted_at.is_(None),
-                        # models.Agent.status == AgentStatus.APPROVED
                     )
                 )
                 .offset(skip)
