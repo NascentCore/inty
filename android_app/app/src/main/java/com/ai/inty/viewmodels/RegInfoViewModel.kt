87a18cebf50749bd1222c2b3a5f6cf9956702f40
GoFeature <gzw@sxwl.ai>
2025-09-28 10:45:56 +0800

    Bug修复，guest游客模式age group在chatinput的拦截，设置后的为更新本地存储的问题

--- android_app/app/src/main/java/com/ai/inty/viewmodels/RegInfoViewModel.kt
+++ android_app/app/src/main/java/com/ai/inty/viewmodels/RegInfoViewModel.kt
@@ -5,6 +5,7 @@ import com.ai.inty.MainActivity
 import com.ai.inty.base.BaseActivityViewModel
 import com.ai.inty.beans.GENDER
 import com.ai.inty.utils.IntyUserProfileSDK
+import com.ai.inty.utils.UnifiedStartupManager.userProfile
 import com.ai.inty.utils.UserProfileManager
 import com.inty.utils.AppEnv
 import com.inty.utils.log.EasyLog
@@ -29,6 +30,10 @@ class RegInfoViewModel : BaseActivityViewModel() {
             if (result != null) {
                 //用户信息更新成功
                 withContext(Dispatchers.Main) {
+                    // 更新本地缓存
+                    UserProfileManager.saveUserProfile(result)
+                    EasyLog.log("Updated user profile from server: $userProfile")
+
                     // 关闭当前设置页面
                     closeActivity()
 
