82acb2f1fd2a2e7e75c22c6111292950ab57d033
ZYX <z@sxwl.ai>
2025-10-12 09:55:31 +0800

    计时器 api (#754)
    Co-authored-by: Yaxiong Zhao <yaxiong.zhao@genesis-ai.company>

--- app/utils/timing.py
+++ app/utils/timing.py
@@ -26,3 +26,21 @@ def log_time(operation_name: str) -> Generator[None, None, None]:
     finally:
         elapsed_time = time.time() - start_time
         logger.debug(f"{operation_name}耗时: {elapsed_time:.3f}秒")
+
+
+class Timeer:
+    """计时器
+    用于记录某个操作的耗时，使用方法：
+    timer = Timeer("测试操作")
+    time.sleep(0.1)
+    mesage = timer.stop()
+    print(mesage)
+    """
+
+    def __init__(self, operation_name: str):
+        self.operation_name = operation_name
+        self.start_time = time.time()
+
+    def stop(self):
+        self.elapsed_time = time.time() - self.start_time
+        return f"[{self.operation_name}] 耗时: {self.elapsed_time:.3f} 秒"
