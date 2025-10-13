532c99b27141f65dcd9077d4d8225b57a4651368
Yaxiong Zhao <zyx@email.com>
2025-10-13 17:27:32 +0800

    Add TestClient for testing Inty APIs

--- tests/app/api/test_client.py
+++ tests/app/api/test_client.py
@@ -1,18 +1,16 @@
-"""
-A minimal client for testing Inty APIs.
-
-It's not auto-generated from OpenAPI spec, but hand rolled, as we do not need
-complex features.
-
-These api will use the same internal data structure for convenience,
-but access Inty backend API through HTTP interface.
-"""
-
 class TestClient:
+    """
+    A minimal client for testing Inty APIs.
+
+    It's not auto-generated from OpenAPI spec, but hand rolled, as we do not need
+    complex features.
+
+    These api will use the same internal data structure for convenience,
+    but access Inty backend API through HTTP interface.
+    """
+
     def __init__(self, base_url: str):
         self.base_url = base_url
 
     def create_guest(self):
-        url = f"{self.base_url}/api/v1/auth/create_guest"
-        response = requests.get(url, params=params)
-        return response.json()
+        pass
