070d6e96ca28636ae0eb364a9905da7e8488adfe
Yaxiong Zhao <z@sxwl.ai>
2025-10-16 16:44:29 +0800

    Fix
    Signed-off-by: Yaxiong Zhao <z@sxwl.ai>

---
+++ devops/test_configs.py
@@ -0,0 +1,17 @@
+"""
+Check both config.yaml.{dev,prod} are valid configs by loading them with config.py
+"""
+
+from app.core.config import _validate_config, load_config
+
+
+def test_load_dev_config():
+    """Test loading dev configuration file."""
+    config = load_config("devops/config.yaml.dev")
+    _validate_config(config)
+
+
+def test_load_prod_config():
+    """Test loading dev configuration file."""
+    config = load_config("devops/config.yaml.prod")
+    _validate_config(config)
