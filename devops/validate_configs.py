2e6e814c5b0b57bead85e59fa94e61d88b75bd78
ZYX <z@sxwl.ai>
2025-10-26 23:25:30 -0700

    移动配置文件模版到 devops 目录下 (#926)

--- devops/validate_configs.py
+++ devops/validate_configs.py
@@ -5,6 +5,12 @@ Check both config.yaml.{dev,prod} are valid configs by loading them with config.
 from app.core.config import _validate_config, load_config
 
 
+def test_load_test_config():
+    """Test loading dev configuration file."""
+    config = load_config("devops/config.yaml.test")
+    _validate_config(config)
+
+
 def test_load_dev_config():
     """Test loading dev configuration file."""
     config = load_config("devops/config.yaml.dev")
