8b4df5b9fad30864d2f0aeab8ce8bc1d8fece5a6
Yaxiong Zhao <z@sxwl.ai>
2025-11-02 00:40:52 -0700

    Fix

--- start.sh
+++ start.sh
@@ -37,6 +37,7 @@ python scripts/init_subscription_plans_simple.py
 if [ "$DEV" = true ]; then
   echo "Starting in development mode..."
   python scripts/init_admin_user.py --user-id user-testing --is-superuser false
+  # 在 CI 环境下下面的命令会导致服务器启动失败
   # ./build_evaluation.sh
   uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload
 else
