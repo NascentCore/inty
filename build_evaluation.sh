cac43b822ff585bcdc42fbc41091c12b2b633fc1
董刚 <donggang@donggangdeMacBook-Pro.local>
2025-11-05 17:24:02 +0800

    fix evaluation build error

--- build_evaluation.sh
+++ build_evaluation.sh
@@ -6,7 +6,7 @@ pushd evaluation/inty_sdk
 # 由于未知原因，tsc-multi 无法被 yarn install 安装，所以手动安装
 yarn add -D tsc-multi@https://github.com/stainless-api/tsc-multi/releases/download/v1.1.9/tsc-multi.tgz
 yarn install # Install dependencies for inty_sdk
-yarn run build # Build inty_sdk, which should create its dist folder
+NODE_OPTIONS="--max-old-space-size=4096" yarn run build # Build inty_sdk, which should create its dist folder
 popd
 
 # 构建 inty-eval 前端，并将其拷贝到 python server 静态资源目录
@@ -18,7 +18,7 @@ npm install
 
 # 构建前端（不设置环境变量，使用相对路径）
 echo "🔨 构建前端应用..."
-npm run build
+NODE_OPTIONS="--max-old-space-size=4096" npm run build
 
 popd
 
