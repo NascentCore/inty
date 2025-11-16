65fca26ce60bff3b2fcaafd7f91e2d4609ad0b66
Yaxiong Zhao <z@sxwl.ai>
2025-11-15 19:10:41 -0800

    Cleanup: Revert tests/app/api/v1/endpoints/conftest.py

--- tests/app/api/v1/endpoints/conftest.py
+++ tests/app/api/v1/endpoints/conftest.py
@@ -5,7 +5,7 @@ import pytest
 from tests.app.api.test_client import TestClient
 
 
-API_BASE_URL = os.getenv("INTY_API_BASE_URL")
+API_BASE_URL = os.getenv("INTY_API_BASE_URL", "http://localhost:8000")
 
 
 @pytest.fixture
