81f2a75148669dff2b1726e62f4bbc2f861ede52
GoFeature <gzw@sxwl.ai>
2025-11-20 15:03:40 +0800

    ui代码的合并

--- android_app/app/src/main/kotlin/com/ai/intellimate/xb/components/WidgetXB.kt
+++ android_app/app/src/main/kotlin/com/ai/intellimate/xb/components/WidgetXB.kt
@@ -1,4 +1,4 @@
-package com.ai.intellimate.ui
+package com.ai.intellimate.xb.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -9,6 +9,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -18,8 +20,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardCapitalization
