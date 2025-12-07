3b8d39a60720ec49a9cf59f895862c2e6b6a6108
Yaxiong Zhao <z@sxwl.ai>
2025-12-07 00:22:11 -0800

    Fix
    Signed-off-by: Yaxiong Zhao <z@sxwl.ai>

--- android_app/app/src/main/kotlin/com/ai/intellimate/explore/ExplorePage.kt
+++ android_app/app/src/main/kotlin/com/ai/intellimate/explore/ExplorePage.kt
@@ -39,7 +39,6 @@ import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.input.pointer.consume
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.paging.LoadState
