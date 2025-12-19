2a5526bc04764db4f9963306b3850900959ab8c7
GoFeature <gzw@sxwl.ai>
2025-12-19 15:01:30 +0800

    优化boost进入chat时，input的文案展示效果 (#1668)

--- android_app/app/src/main/kotlin/com/ai/intellimate/chat/ui/ChatInput.kt
+++ android_app/app/src/main/kotlin/com/ai/intellimate/chat/ui/ChatInput.kt
@@ -84,15 +84,15 @@ fun ChatInput(
                 modifier = Modifier.padding(end = TrailingControlsPadding).align(Alignment.Center),
                 value = inputData.value,
                 singleLine = false,
-                placeholder = {
-                    val defaultName = stringResource(R.string.chat_ai_typing_default_name)
-                    val targetName = agentInfo.firstNameOrNull() ?: defaultName
-
-                    Text(
-                        text = stringResource(R.string.chat_input_placeholder, targetName),
-                        fontSize = 14.sp,
-                        color = Color.White.copy(alpha = 0.5f),
-                    )
+                placeholder = agentInfo?.let {
+                    {
+                        val targetName = it.firstNameOrNull() ?: stringResource(R.string.chat_ai_typing_default_name)
+                        Text(
+                            text = stringResource(R.string.chat_input_placeholder, targetName),
+                            fontSize = 14.sp,
+                            color = Color.White.copy(alpha = 0.5f),
+                        )
+                    }
                 },
                 onValueChange = { input -> chatViewModel.inputData.value = input },
                 keyboardOptions =
