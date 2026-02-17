350ce0a82b380e212006578b60f42d7720489319
Yaxiong Zhao <zyx@email.com>
2026-02-17 00:07:30 -0800

    添加详细的模型配置记录，方便规整各类模型
    Signed-off-by: Yaxiong Zhao <zyx@email.com>

--- app/utils/config.py
+++ app/utils/config.py
@@ -10,6 +10,8 @@ from pydantic import AnyHttpUrl
 
 from loguru import logger
 
+from app.utils import models_catalog
+
 # 所有配置项必须有默认值，防止出现校验失败。
 # 这些默认值允许在没有实际配置文件的情况下使用。
 # 因为 config 对象被用作全局单例，大部分代码依赖它，但并不实际使用配置值，所以默认值是可以的。
@@ -21,15 +23,11 @@ from loguru import logger
 # 3. 【如有必要】删除该配置项在 app 客户端相关的使用，部署、发布验证一切正常。
 
 OPENROUTER_BASE_URL = "https://openrouter.ai/api/v1"
-# 3X of lite: $0.30	$0.30	$0.030	$0.030	$0.15	$0.15
-GEMINI_2_5_FLASH = "google/gemini-2.5-flash"
-# $0.1	$0.1	$0.010	$0.010	$0.05	$0.05
-GEMINI_2_5_FLASH_LITE = "google/gemini-2.5-flash-lite"
 
-# 3X of fast: $0.06 per image
-VERTEX_AI_IMAGEN_4_ULTRA = "imagen-4.0-ultra-generate-001"
-# 0.02 per image, 这个可以调整为更弱的模型
-VERTEX_AI_IMAGEN_4_FAST = "imagen-4.0-fast-generate-001"
+GEMINI_2_5_FLASH = models_catalog.GEMINI_2_5_FLASH.id_on_provider
+GEMINI_2_5_FLASH_LITE = models_catalog.GEMINI_2_5_FLASH_LITE.id_on_provider
+VERTEX_AI_IMAGEN_4 = models_catalog.IMAGEN_4.id_on_provider
+VERTEX_AI_IMAGEN_4_FAST = models_catalog.IMAGEN_4_FAST.id_on_provider
 
 
 class Environment(str, Enum):
@@ -236,7 +234,7 @@ class AgentConfig:
     enable_debug_logging: bool = False  # 是否启用调试日志记录功能
     vertex_image_model: str = VERTEX_AI_IMAGEN_4_FAST
     free_user_text_to_image_model: str = VERTEX_AI_IMAGEN_4_FAST
-    sub_user_text_to_image_model: str = VERTEX_AI_IMAGEN_4_ULTRA
+    sub_user_text_to_image_model: str = VERTEX_AI_IMAGEN_4
     force_default_prompts: bool = False  # 强制使用默认提示词，忽略Agent自定义提示词
     enable_christmas_prompt: bool = False  # 是否启用圣诞节季节性提示词
     # 图片生成配置
