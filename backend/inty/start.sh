b5de669c19dd6265db72b4eb4f45f4eb035b359b
ZYX <z@sxwl.ai>
2026-03-04 15:34:59 +0800

    Update start.sh to create test user instead of admin
    Comment out old admin user initialization and add new test user creation script.

--- backend/inty/start.sh
+++ backend/inty/start.sh
@@ -63,7 +63,10 @@ if [ "$DEV" = true ]; then
   else
     echo "Starting in dev mode..."
   fi
-  python scripts/init_admin_user.py --user-id user-testing --is-superuser=true
+  # 生成测试用管理员账号，ops 平台与 inty 后端分离后，这个应该就不需要了，先注释掉保留来做记录。
+  # python scripts/init_admin_user.py --user-id user-testing --is-superuser=true
+  # 生成测试用户用于本地 app 登陆
+  python scripts/create_email_password_superuser.py --email test@sxwl.ai --password test --yes
   uvicorn backend.inty.main:app --host 0.0.0.0 --port 8000 --reload
 else
   echo "Starting in normal mode without reloading..."
