ba727e09defa6aefcdaa2bb51e5b887169c9e477
kotlinai <70425737+kotlinai@users.noreply.github.com>
2026-04-13 16:35:47 +0800

    添加签名(#2821)
    Co-authored-by: kotlinai <kotlinaai@hotmail.com>

--- imate_android_app/core/src/main/java/com/ai/core/http/utils/HttpRequestExt.kt
+++ imate_android_app/core/src/main/java/com/ai/core/http/utils/HttpRequestExt.kt
@@ -4,7 +4,6 @@ import com.ai.core.data.bean.HttpResult
 import com.ai.core.data.exceptions.IntyException
 import com.ai.core.http.HttpErrorHandler
 import com.ai.core.http.di.KtorHttpClientSingleton
-import io.ktor.client.plugins.HttpRequestTimeoutException
 import io.ktor.client.request.HttpRequestBuilder
 import io.ktor.client.request.get
 import io.ktor.client.request.post
@@ -12,17 +11,11 @@ import io.ktor.client.request.put
 import io.ktor.client.statement.HttpResponse
 
 @PublishedApi
-internal fun Throwable.toIntyException(operation: String, code: Int = -1): IntyException {
-    val msg = HttpErrorHandler.toUserMessage(this, operation)
-    return IntyException(code, msg)
-}
-
-@PublishedApi
 internal fun <T : Any> unwrapHttpResultOrThrow(result: HttpResult<T>): T {
     if (result.code != 200) {
         throw IntyException(result.code, result.message)
     }
-    return result.data ?: throw IntyException(HttpResult.ErrorCode.EmptyResponse.value, "Empty response")
+    return result.data ?: throw IllegalStateException("Empty response")
 }
 
 @PublishedApi
@@ -35,88 +28,52 @@ suspend inline fun <reified T : Any> post(
     url: String,
     noinline block: HttpRequestBuilder.() -> Unit = {},
 ): T {
-    try {
-        val response = KtorHttpClientSingleton.httpClient.post(url, block)
-        val result = HttpErrorHandler.parseHttpResultOrFailure<T>(response, KtorHttpClientSingleton.ktorHttpJson)
-        return unwrapHttpResultOrThrow(result)
-    } catch (t: HttpRequestTimeoutException) {
-        throw t.toIntyException("post $url")
-    } catch (t: Throwable) {
-        throw t.toIntyException("post $url")
-    }
+    val response = KtorHttpClientSingleton.httpClient.post(url, block)
+    val result = HttpErrorHandler.parseHttpResultOrFailure<T>(response, KtorHttpClientSingleton.ktorHttpJson)
+    return unwrapHttpResultOrThrow(result)
 }
 
 suspend inline fun <reified T : Any> get(
     url: String,
     noinline block: HttpRequestBuilder.() -> Unit = {},
 ): T {
-    try {
-        val response = KtorHttpClientSingleton.httpClient.get(url, block)
-        val result = HttpErrorHandler.parseHttpResultOrFailure<T>(response, KtorHttpClientSingleton.ktorHttpJson)
-        return unwrapHttpResultOrThrow(result)
-    } catch (t: HttpRequestTimeoutException) {
-        throw t.toIntyException("get $url")
-    } catch (t: Throwable) {
-        throw t.toIntyException("get $url")
-    }
+    val response = KtorHttpClientSingleton.httpClient.get(url, block)
+    val result = HttpErrorHandler.parseHttpResultOrFailure<T>(response, KtorHttpClientSingleton.ktorHttpJson)
+    return unwrapHttpResultOrThrow(result)
 }
 
 suspend inline fun <reified T : Any> put(
     url: String,
     noinline block: HttpRequestBuilder.() -> Unit = {},
 ): T {
-    try {
-        val response = KtorHttpClientSingleton.httpClient.put(url, block)
-        val result = HttpErrorHandler.parseHttpResultOrFailure<T>(response, KtorHttpClientSingleton.ktorHttpJson)
-        return unwrapHttpResultOrThrow(result)
-    } catch (t: HttpRequestTimeoutException) {
-        throw t.toIntyException("put $url")
-    } catch (t: Throwable) {
-        throw t.toIntyException("put $url")
-    }
+    val response = KtorHttpClientSingleton.httpClient.put(url, block)
+    val result = HttpErrorHandler.parseHttpResultOrFailure<T>(response, KtorHttpClientSingleton.ktorHttpJson)
+    return unwrapHttpResultOrThrow(result)
 }
 
 suspend inline fun <reified T : Any> getRaw(
     url: String,
     noinline block: HttpRequestBuilder.() -> Unit = {},
 ): T {
-    try {
-        val response = KtorHttpClientSingleton.httpClient.get(url, block)
-        val result = HttpErrorHandler.parseRawBodyOrFailure<T>(response, KtorHttpClientSingleton.ktorHttpJson)
-        return unwrapHttpResultOrThrow(result)
-    } catch (t: HttpRequestTimeoutException) {
-        throw t.toIntyException("get $url")
-    } catch (t: Throwable) {
-        throw t.toIntyException("get $url")
-    }
+    val response = KtorHttpClientSingleton.httpClient.get(url, block)
+    val result = HttpErrorHandler.parseRawBodyOrFailure<T>(response, KtorHttpClientSingleton.ktorHttpJson)
+    return unwrapHttpResultOrThrow(result)
 }
 
 suspend inline fun <reified T : Any> postRaw(
     url: String,
     noinline block: HttpRequestBuilder.() -> Unit = {},
 ): T {
-    try {
-        val response = KtorHttpClientSingleton.httpClient.post(url, block)
-        val result = HttpErrorHandler.parseRawBodyOrFailure<T>(response, KtorHttpClientSingleton.ktorHttpJson)
-        return unwrapHttpResultOrThrow(result)
-    } catch (t: HttpRequestTimeoutException) {
-        throw t.toIntyException("post $url")
-    } catch (t: Throwable) {
-        throw t.toIntyException("post $url")
-    }
+    val response = KtorHttpClientSingleton.httpClient.post(url, block)
+    val result = HttpErrorHandler.parseRawBodyOrFailure<T>(response, KtorHttpClientSingleton.ktorHttpJson)
+    return unwrapHttpResultOrThrow(result)
 }
 
 suspend inline fun <reified T : Any> putRaw(
     url: String,
     noinline block: HttpRequestBuilder.() -> Unit = {},
 ): T {
-    try {
-        val response = KtorHttpClientSingleton.httpClient.put(url, block)
-        val result = HttpErrorHandler.parseRawBodyOrFailure<T>(response, KtorHttpClientSingleton.ktorHttpJson)
-        return unwrapHttpResultOrThrow(result)
-    } catch (t: HttpRequestTimeoutException) {
-        throw t.toIntyException("put $url")
-    } catch (t: Throwable) {
-        throw t.toIntyException("put $url")
-    }
+    val response = KtorHttpClientSingleton.httpClient.put(url, block)
+    val result = HttpErrorHandler.parseRawBodyOrFailure<T>(response, KtorHttpClientSingleton.ktorHttpJson)
+    return unwrapHttpResultOrThrow(result)
 }
