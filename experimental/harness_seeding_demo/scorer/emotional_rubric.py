79e36d990793f375de9472a3bffac9e9b59fc178
Yaxiong Zhao <zyx@email.com>
2026-05-29 17:17:21 +0800

    Remove low value experiment
    Signed-off-by: Yaxiong Zhao <zyx@email.com>

--- experimental/harness_seeding_demo/scorer/emotional_rubric.py
+++
@@ -1,8 +0,0 @@
-"""Backward-compatible single-rubric module."""
-
-from experimental.harness_seeding_demo.scorer.rubrics import (
-    ScoreResult,
-    score_emotional_understanding_reply,
-)
-
-__all__ = ["ScoreResult", "score_emotional_understanding_reply"]
