11021efc5ed5e71a50c3d808a49c93b71398a389
ZYX <z@sxwl.ai>
2026-06-20 19:37:34 -0700

    Delete app/core/llm_client.py

--- app/core/llm_client.py
+++
@@ -1,7 +0,0 @@
-"""
-LLM Client for calling LLM APIs.
-Centralize all LLM calient APIs in this module.
-
-TODO(merge-llm-client): Move core.companion_harness.companion.llm_client here,
-and merge old utils.openai_client into this module.
-"""
